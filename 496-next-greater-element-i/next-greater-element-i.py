a6982254599f4d9c56eed2e61867985884c8306d
Pruthvi Singh <pruthvisinghp25@gmail.com>
2026-08-03 22:32:50 +0530

    Time: 3 ms (52.61%) | Memory: 12.5 MB (91.73%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,25 +1,71 @@
 class Solution(object):
     def nextGreaterElement(self, nums1, nums2):
-        grater_element = []
+        stack = []
+        freq = {}
+      
+        for i in range(len(nums2)-1, -1, -1):
+            while stack and stack[-1] <= nums2[i]:
+                stack.pop()
+
+            if not stack:
+                freq[nums2[i]] = -1
+            else:
+                freq[nums2[i]] = stack[-1]
+                
+            stack.append(nums2[i])
+
+        ans = []
 
         for i in nums1:
+            if i in freq:
+                ans.append(freq[i])
+        
+        return ans
+
             
-            idx = nums2.index(i)
+
             
-            # found = False
+
             
-            for j in range(idx + 1, len(nums2)):
             
-                if i < nums2[j]:
-                     grater_element.append(nums2[j])
-                    #  found = True
-                     break
+
+
+
             
-            # if not found:
-            else:
-                grater_element.append(-1)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # [brutal foarce approach]
+        # grater_element = []
+
+        # for i in nums1:
+            
+        #     idx = nums2.index(i)
+            
+        #     # found = False
+            
+        #     for j in range(idx + 1, len(nums2)):
+            
+        #         if i < nums2[j]:
+        #              grater_element.append(nums2[j])
+        #             #  found = True
+        #              break
+            
+        #     # if not found:
+        #     else:
+        #         grater_element.append(-1)
                    
-        return grater_element
+        # return grater_element
                 
             
 
