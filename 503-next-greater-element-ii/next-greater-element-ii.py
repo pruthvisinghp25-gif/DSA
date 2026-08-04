385551a82362ead3998888580c1f1774ce055f2d
Pruthvi Singh <pruthvisinghp25@gmail.com>
2026-08-04 22:13:36 +0530

    Time: 419 ms (7.58%) | Memory: 21.4 MB (10.12%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,16 +1,42 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [-1] * n
-        stack = []
+        s = []
+        ngn = {}
+        i = 0
+        
+        while len(ngn.keys()) != len(nums):
+            cur = nums[i]
+            if s and i == s[-1]:
+                ngn[i] = -1
+                s.pop()
+            else:
+                if s:
+                    while s and cur > nums[s[-1]]:
+                        ngn[s.pop()] = cur
+                if i not in ngn and i not in s:
+                    s.append(i)
+            
+            i = (i + 1)%len(nums)
+        
+        print(ngn)
+        
+        result = []
+        for j in range(len(nums)):
+            result.append(ngn[j])
+        return result
+# class Solution:
+#     def nextGreaterElements(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
+#         ans = [-1] * n
+#         stack = []
 
-        for i in range(2*n-1, -1, -1):
-            while stack and stack[-1] <= nums[i % n]:
-                stack.pop()
+#         for i in range(2*n-1, -1, -1):
+#             while stack and stack[-1] <= nums[i % n]:
+#                 stack.pop()
                 
-            if stack:
-                ans[i % n] = stack[-1]
+#             if stack:
+#                 ans[i % n] = stack[-1]
 
-            stack.append(nums[i % n])
+#             stack.append(nums[i % n])
 
-        return ans
+#         return ans
